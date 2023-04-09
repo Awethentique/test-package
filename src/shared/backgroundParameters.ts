9ad55fb7f6ce11090dc66c85df20cd49cf197f55
Awethentique <ritesh.makan@gmail.com>
2023-04-10 00:08:09 +0200

    adding addons
        'storybook-addon-designs',
        '@storybook/addon-a11y',
        '@storybook/addon-interactions',
        '@storybook/addon-coverage',

---
+++ src/shared/backgroundParameters.ts
@@ -0,0 +1,25 @@
+export const backgroundParameters = {
+  default: 'light',
+  values: [
+    {name: 'bottom sheet light', value: '#FFFFFF'},
+    {name: 'bottom sheet dark', value: '#000000'},
+    {name: 'light', value: '#FFFBFF'},
+    {name: 'dark', value: '#1C1B1E'},
+  ],
+};
+
+export const appBackground = {
+  default: 'light',
+  values: [
+    {name: 'light', value: '#FFFBFF'},
+    {name: 'dark', value: '#1C1B1E'},
+  ],
+};
+
+export const bottomSheetBackground = {
+  default: 'light',
+  values: [
+    {name: 'light', value: '#FFFFFF'},
+    {name: 'dark', value: '#000000'},
+  ],
+};
