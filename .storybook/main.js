bef3d0f9f36b556c36f304e59acf10b71c82cdbd
Awethentique <ritesh.makan@gmail.com>
2023-04-10 16:12:00 +0200

    Add Chromatic

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,14 @@
+
 module.exports = {
-  stories: ['../src/components/**/*.stories.?(ts|tsx|js|jsx)'],
+  stories: [
+    // '../src/**/*.mdx',
+    '../src/components/**/*.stories.?(ts|tsx|js|jsx)'
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     'storybook-addon-designs',
+    // '@storybook/addon-docs',
     '@storybook/addon-a11y',
     '@storybook/addon-interactions',
     '@storybook/addon-coverage',
@@ -13,6 +18,34 @@ module.exports = {
     interactionsDebugger: true,
   },
   framework: '@storybook/react',
+  // webpackFinal: async (config, { configType }) => {
+  //   // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  //   // You can change the configuration based on that.
+  //   // 'PRODUCTION' is used when building the static version of storybook.
+  //   // Make whatever fine-grained changes you need
+  //   config.module.rules.push({
+  //     test: /\.less$/,
+  //     use: [
+  //       "style-loader",
+  //       "css-loader",
+  //       "sass-loader",
+  //       {
+  //         loader: "less-loader",
+  //         // compiles Less to CSS
+  //         options: {
+  //           lessOptions: {
+  //             // If you are using less-loader@5 please spread the lessOptions to options directly
+  //             //  modifyVars: themeVariables,
+  //             javascriptEnabled: true,
+  //             sourceMap: true,
+  //           },
+  //         },
+  //       },
+  //     ],
+  //   }); // Return the altered config
+
+  //   return config;
+  // },
   core: {
     builder: 'webpack5',
   },
